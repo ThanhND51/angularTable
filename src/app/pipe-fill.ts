ae919ee7b6e3c3e1c4630039839d3e5c74245875
A23611 <41458791+A23611@users.noreply.github.com>
2019-02-12 14:46:37 +0700

    up

--- src/app/pipe-fill.ts
+++ src/app/pipe-fill.ts
@@ -3,11 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'fillter' })
 
 export class PipeFill implements PipeTransform {
-    transform(array: [], _k: any): any {
+    transform(array: [], k: any): any {
         array.sort((a: any, b: any) => {
-            if (a < b) {
+            if (a[k] < b[k]) {
                 return -1;
-            } else if (a > b) {
+            } else if (a[k] > b[k]) {
                 return 1;
             } else {
                 return 0;
